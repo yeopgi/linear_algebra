c777d7c7e6c77a25713f1d03095b1472484b40b7
yeopgi <7677942@hanmail.net>
2021-11-08 11:23:22 +0900

    working...

---
+++ c++/reduced_echelon_form_matrix.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int reduced_echelon_form_matrix[5][4] =
+    {{1, 0, 4, 1},
+     {0, 1, 0, 0},
+     {0, 0, 1, 4},
+     {0, 0, 0, 0},
+     {0, 0, 0, 0}};
+
+int main(void)
+{
+
+    return 0;
+}
